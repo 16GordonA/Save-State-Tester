d4256ad9a907fd36e97d0e104557e6812852eaca
16GordonA <16gordona@my.weston.org>
2015-01-19 08:57:11 -0500

    Started Coding

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import pygame, sys, random, time, pygame.mixer, pygame.font
+from pygame.locals import *
+from pygame.font import *
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 450
+
+pygame.font.init()
+myFont = pygame.font.SysFont("Comic Sans", 18)
+
+size = SCREEN_WIDTH, SCREEN_HEIGHT
+screen = pygame.display.set_mode(size)
+
+while True:
+    screen.
+    key = pygame.key.get_pressed()
+    
+    #340,230
+
 #ask for name
